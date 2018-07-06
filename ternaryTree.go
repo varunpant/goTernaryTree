a27a03c08bbe48e60ea7cc9c895f2fc679ddb1bf
varun pant <varun.pant@karhoo.com>
2018-07-06 16:24:34 +0100

    initial commit

---
+++ ternaryTree.go
@@ -0,0 +1,156 @@
+package goTernaryTree
+
+import (
+	"errors"
+)
+
+// Item represents a single object in the Node.
+type Item interface{}
+
+// Node represents a single object in the tree.
+type Node struct {
+	c                uint8
+	left, mid, right *Node
+	value            Item
+}
+
+//Ternary Tree
+type TernaryTree struct {
+	size int
+	root *Node
+}
+
+//Creates new instance of TernaryTree
+func NewTernaryTree() *TernaryTree {
+	return &TernaryTree{size: 0}
+}
+
+//Adds new key to the tree.
+func (t *TernaryTree) add(key string, value Item) {
+
+	if !t.contains(key) {
+		t.size++
+		t.root = insert(t.root, key, value, 0)
+	}
+
+}
+
+//Searches if tree contains key.
+func (t *TernaryTree) contains(key string) bool {
+	node := search(t.root, key, 0)
+	if node == nil {
+		return false
+	}
+
+	return true
+}
+
+//Gets saved key from the tree.
+func (t *TernaryTree) get(key string) (Item, error) {
+
+	node := search(t.root, key, 0)
+	if node == nil {
+		return nil, errors.New("Key not found.")
+	}
+	return node.value, nil
+}
+
+//Searches tree for keys with common prefix.
+func (t *TernaryTree) prefixMatch(prefix string) []Item {
+	if t.root == nil {
+		return nil
+	}
+	bucket := []Item{}
+	node := search(t.root, prefix, 0)
+	if node == nil {
+		return nil
+	}
+	if node.value != nil {
+		bucket = append(bucket, prefix)
+	}
+	collect(node.mid, prefix, &bucket)
+	return bucket
+}
+
+//Searches tree with a wild card to match  keys.
+func (t *TernaryTree) wildcardMatch(pattern string) []Item {
+	if t.root == nil {
+		return nil
+	}
+	bucket := []Item{}
+	collect2(t.root, "", 0, pattern, &bucket)
+	return bucket
+}
+
+func search(node *Node, key string, charIndex int) *Node {
+	if node == nil {
+		return nil
+	}
+	c := key[charIndex]
+	if c < node.c {
+		return search(node.left, key, charIndex)
+	} else if c > node.c {
+		return search(node.right, key, charIndex)
+	} else if charIndex < len(key)-1 {
+		return search(node.mid, key, charIndex+1)
+	} else {
+		return node
+	}
+}
+
+func insert(node *Node, key string, value Item, charIndex int) *Node {
+	ci := key[charIndex]
+
+	if node == nil {
+		node = &Node{c: ci}
+	}
+
+	if ci < node.c {
+		node.left = insert(node.left, key, value, charIndex)
+	} else if ci > node.c {
+		node.right = insert(node.right, key, value, charIndex)
+	} else if charIndex < len(key)-1 {
+		node.mid = insert(node.mid, key, value, charIndex+1)
+	} else {
+		node.value = value
+	}
+	return node
+}
+
+func collect(node *Node, prefix string, bucket *[]Item) {
+
+	if node == nil {
+		return
+	}
+	collect(node.left, prefix, bucket)
+	if node.value != nil {
+		*bucket = append(*bucket, prefix+string(node.c))
+	}
+	collect(node.mid, prefix+string(node.c), bucket)
+	collect(node.right, prefix, bucket)
+}
+
+func collect2(node *Node, prefix string, charIndex int, pattern string, bucket *[]Item) {
+	if node == nil {
+		return
+	}
+	ci := pattern[charIndex]
+
+	if ci == '.' || ci < node.c {
+		collect2(node.left, prefix, charIndex, pattern, bucket)
+	}
+
+	if ci == '.' || ci == node.c {
+		if charIndex == len(pattern)-1 && node.value != nil {
+			*bucket = append(*bucket, prefix+string(node.c))
+		}
+		if charIndex < len(pattern)-1 {
+			collect2(node.mid, prefix+string(node.c), charIndex+1, pattern, bucket)
+		}
+	}
+
+	if ci == '.' || ci > node.c {
+		collect2(node.right, prefix, charIndex, pattern, bucket)
+	}
+
+}
---
+++ ternaryTree_test.go
@@ -0,0 +1,164 @@
+package goTernaryTree
+
+import (
+	"bufio"
+	"os"
+	"testing"
+)
+
+func TestCreateTernaryTree(t *testing.T) {
+	ttree := NewTernaryTree()
+	inputKeys := [12]string{"aback", "abacus", "abalone", "abandon", "abase", "abash", "abate", "abbas", "abbe", "abbey", "abbot", "Abbott"}
+
+	for _, key := range inputKeys {
+		ttree.add(key, key)
+	}
+
+	if ttree == nil {
+		t.Error("Failed to create terenaryTree")
+	}
+
+	if ttree.size != 12 {
+		t.Errorf("Expected size to be %d, but got %d", 12, ttree.size)
+	}
+
+	for _, key := range inputKeys {
+
+		value, _ := ttree.get(key)
+		if key != value {
+			t.Errorf("Failed to get the valid value for key %s from tree, recieved %s", key, value)
+		}
+	}
+}
+
+func TestPrefixSearch(t *testing.T) {
+	ttree := NewTernaryTree()
+	inputKeys := [12]string{"aback", "abacus", "abalone", "abandon", "abase", "abash", "abate", "abbas", "abbe", "abbey", "abbot", "Abbott"}
+
+	for _, key := range inputKeys {
+		ttree.add(key, key)
+	}
+
+	hits := ttree.prefixMatch("aba")
+
+	if len(hits) != 7 {
+		t.Errorf("incorect prefix search hits from tree, expected 7, but recieved %d", len(hits))
+	}
+
+	expectedHits := [7]string{"aback", "abacus", "abalone", "abandon", "abase", "abash", "abate"}
+	for idx, hit := range hits {
+		if hit != expectedHits[idx] {
+			t.Errorf("expected search hit %s, but got %s", hit, expectedHits[idx])
+		}
+	}
+}
+
+func TestWildcardMatch(t *testing.T) {
+	ttree := NewTernaryTree()
+	inputKeys := [12]string{"aback", "abacus", "abalone", "abandon", "abase", "abash", "abate", "abbas", "abbe", "abbey", "abbot", "abbott"}
+
+	for _, key := range inputKeys {
+		ttree.add(key, key)
+	}
+
+	{
+		matches := ttree.wildcardMatch("...cus")
+		if len(matches) != 1 {
+			t.Errorf("Incorrect match count, expected %d, but got %d", 1, len(matches))
+		}
+		if matches[0] != "abacus" {
+			t.Errorf("Incorrect match, expected %s, but got %s", "abacus", matches[0])
+		}
+	}
+
+	{
+		matches := ttree.wildcardMatch("....y")
+		if len(matches) != 1 {
+			t.Errorf("Incorrect match count, expected %d, but got %d", 1, len(matches))
+		}
+		if matches[0] != "abbey" {
+			t.Errorf("Incorrect match, expected %s, but got %s", "abbey", matches[0])
+		}
+	}
+
+	{
+		matches := ttree.wildcardMatch("a.b.t.")
+		if len(matches) != 1 {
+			t.Errorf("Incorrect match count, expected %d, but got %d", 1, len(matches))
+		}
+		if matches[0] != "abbott" {
+			t.Errorf("Incorrect match, expected %s, but got %s", "abbott", matches[0])
+		}
+	}
+
+	{
+		matches := ttree.wildcardMatch("aba...")
+		if len(matches) != 1 {
+			t.Errorf("Incorrect match count, expected %d, but got %d", 1, len(matches))
+		}
+		if matches[0] != "abacus" {
+			t.Errorf("Incorrect match, expected %s, but got %s", "abacus", matches[0])
+		}
+	}
+
+	{
+		matches := ttree.wildcardMatch("..a..")
+		if len(matches) != 4 {
+			t.Errorf("Incorrect match count, expected %d, but got %d", 4, len(matches))
+		}
+		if matches[0] != "aback" {
+			t.Errorf("Incorrect match, expected %s, but got %s", "aback", matches[0])
+		}
+		if matches[3] != "abate" {
+			t.Errorf("Incorrect match, expected %s, but got %s", "abate", matches[0])
+		}
+	}
+
+}
+
+func BenchmarkTernaryTreeInsert(b *testing.B) {
+
+	//370099 keys
+	words, err := readWords("words_alpha.txt")
+	if err != nil {
+		panic("file not found")
+	}
+
+	ttree := NewTernaryTree()
+	b.ResetTimer()
+	b.Run(`InsertKeys`, func(b *testing.B) {
+		for _, key := range words {
+			ttree.add(key, key)
+		}
+
+	})
+
+	b.Run(`RetrieveKeys`, func(b *testing.B) {
+		for _, key := range words {
+			value, err := ttree.get(key)
+			if err != nil {
+				b.Errorf("Recieved error while getting %s,%s", key, err)
+			}
+			if key != value {
+				b.Errorf("Failed to get the valid value for the key %s ,recieved %s", key, value)
+			}
+		}
+
+	})
+
+}
+
+func readWords(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
